0562ab859f5992d6cc9d9e5e9fc91293b51b3fd8
Hong <1172406997@qq.com>
2021-02-24 18:51:50 +0800

    打包文件处理

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,19 @@
 const path = require('path');
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 // css加载插件
-const miniCssExtractPlugin=require('mini-css-extract-plugin');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 // 静态页面模版
 const hwp = [
   new HtmlWebpackPlugin({
     filename: "slider-animate.html",
-    template: path.join(__dirname, "..", "", "./components/slider/slider-animate/slider-animate.html"),
+    template: path.join(__dirname, "./components/slider/slider-animate/slider-animate.html"),
     title: "首页",
-    chunks: ["slider-animate"], // 使用哪些打包的js文件
+    // chunks: ["slider-animate"], // 使用哪些打包的js文件
+    inject : 'body',
+    inlineSource : '.(js|css)$', //全部内嵌
     minify: {
       removeComments: false,
       collapseWhitespace: false,
@@ -53,8 +56,13 @@ module.exports = {
     {
         test: /\.css$/,
         use: [
+          // 将css抽分为单独文件
           {
-            loader:miniCssExtractPlugin.loader  //抽离成一个css文件
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "../../",
+              plugins: [require("autoprefixer")],
+            },
           },
           { loader: 'style-loader' },
           { loader: 'css-loader' },
@@ -72,11 +80,13 @@ module.exports = {
     quiet: true, // 禁止掉devServer的console.log信息
   },
   plugins: [
+    // dist 目录清理
+    // new CleanWebpackPlugin(),
     // 静态页面模版
     ...hwp,
     // 将css进行输出
-    new miniCssExtractPlugin({
-      filename:'css/[name].css'   //输出的css文件名，放置在dist目录下
+    new MiniCssExtractPlugin({
+      filename:'css/[name].[chunkhash].css'   //输出的css文件名，放置在dist目录下
     }),
     // 自定义webpack的消息提示
     new FriendlyErrorsWebpackPlugin({
